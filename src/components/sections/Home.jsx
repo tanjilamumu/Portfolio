bf5ed4705a0aa14d0737687e4f5b280bb9fa971f
tanjilamumu <tanjilamumu@ymail.com>
2025-11-19 23:35:55 -0500

    fix

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -54,12 +54,13 @@ export const Home = () => {
             </a>
 
             <a
-              href="/newresume.docx" 
+              href="/Portfolio/New Resume.docx"
               target="_blank"
               className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
-              >
-                View Resume
-              </a>
+            >
+              View Resume
+            </a>
+
 
               <a
                 href="https://github.com/tanjilamumu" 
